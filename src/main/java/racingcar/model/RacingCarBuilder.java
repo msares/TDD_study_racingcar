60c0681fc52fad3b930927789cc1431e1a399cea
hana.kim <hana.kim@datastreams.co.kr>
2022-11-27 14:01:35 +0900

    -feat(RacingCarBuilder , RacingModule , UserInputModule ) : fix indent

--- src/main/java/racingcar/model/RacingCarBuilder.java
+++ src/main/java/racingcar/model/RacingCarBuilder.java
@@ -1,45 +1,44 @@
 package racingcar.model;
 
 import org.junit.platform.commons.util.StringUtils;
-import racingcar.model.RacingCar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarBuilder {
-    private static final int MAX_CAR_NAME_LENGTH = 5;
-    private static final String DELIMITER = ",";
-    private static final String ERROR_PREFIX = "[ERROR]";
-    private static final String BLANK_STRING_ERROR = ERROR_PREFIX + " Car name cannot be empty string.";
-    private static final String OUT_OF_BOUND_ERROR = ERROR_PREFIX + " Max length of car name is " + MAX_CAR_NAME_LENGTH + ".";
+  private static final int MAX_CAR_NAME_LENGTH = 5;
+  private static final String DELIMITER = ",";
+  private static final String ERROR_PREFIX = "[ERROR]";
+  private static final String BLANK_STRING_ERROR = ERROR_PREFIX + " Car name cannot be empty string.";
+  private static final String OUT_OF_BOUND_ERROR = ERROR_PREFIX + " Max length of car name is " + MAX_CAR_NAME_LENGTH + ".";
 
-    private final String carNames;
+  private final String carNames;
 
-    public RacingCarBuilder(String carNames) {
-        this.carNames = carNames;
+  public RacingCarBuilder(String carNames) {
+    this.carNames = carNames;
+  }
+
+  public List<RacingCar> createCars() {
+    List<RacingCar> cars = new ArrayList<>();
+
+    for (String carName : carNames.split(DELIMITER)) {
+      String name = carName.trim();
+      try {
+        checkCarName(name);
+      } catch (RuntimeException e) {
+      }
+      cars.add(new RacingCar(name));
     }
 
-    public List<RacingCar> createCars() {
-        List<RacingCar> cars = new ArrayList<>();
+    return cars;
+  }
 
-        for (String carName : carNames.split(DELIMITER)) {
-            String name = carName.trim();
-            try {
-                checkCarName(name);
-            } catch (RuntimeException e) {
-            }
-            cars.add(new RacingCar(name));
-        }
-
-        return cars;
+  private void checkCarName(String name) {
+    if (StringUtils.isBlank(name)) {
+      throw new RuntimeException(BLANK_STRING_ERROR, new IllegalArgumentException());
     }
-
-    private void checkCarName(String name) {
-        if (StringUtils.isBlank(name)) {
-            throw new RuntimeException(BLANK_STRING_ERROR, new IllegalArgumentException());
-        }
-        if (name.length() > MAX_CAR_NAME_LENGTH) {
-            throw new RuntimeException(OUT_OF_BOUND_ERROR, new IllegalArgumentException());
-        }
+    if (name.length() > MAX_CAR_NAME_LENGTH) {
+      throw new RuntimeException(OUT_OF_BOUND_ERROR, new IllegalArgumentException());
     }
+  }
 }
--- src/main/java/racingcar/view/UserInputModule.java
+++ src/main/java/racingcar/view/UserInputModule.java
@@ -2,16 +2,16 @@ package racingcar.view;
 
 import java.util.Scanner;
 
-public  class UserInputModule {
-    private static final Scanner scanner = new Scanner(System.in);
+public class UserInputModule {
+  private static final Scanner scanner = new Scanner(System.in);
 
-    public static String inputCarNames() {
-        System.out.println("경주할자동차이름(이름은쉼표(,)기준으로구분)");
-        return scanner.nextLine();
-    }
+  public static String inputCarNames() {
+    System.out.println("경주할자동차이름(이름은쉼표(,)기준으로구분)");
+    return scanner.nextLine();
+  }
 
-    public static int inputTryCount() {
-        System.out.println("시도할 회수");
-        return scanner.nextInt();
-    }
+  public static int inputTryCount() {
+    System.out.println("시도할 회수");
+    return scanner.nextInt();
+  }
 }
