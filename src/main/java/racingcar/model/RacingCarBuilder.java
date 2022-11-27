9e258111a5c049cf424b3d539958a51a2cf3c02b
hana.kim <hana.kim@datastreams.co.kr>
2022-11-27 19:53:48 +0900

    -feat( RacingCarBuilder ) : 자동차 객체 생성을 위한 클래스 구현

---
+++ src/main/java/racingcar/model/RacingCarBuilder.java
@@ -0,0 +1,20 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static racingcar.common.GameConstants.DELIMITER;
+
+public class RacingCarBuilder {
+  public RacingCarBuilder() { // 모든 클래스는 반드시 하나이상의 생성자를 가져야 함. 개발자가 별도의 생성자를 작성하지 않을 경우 컴파일러가 빈 생성자(파라미터로 아무것도 받지 않는 생성자)를 추가하도록 설정되어 있음.
+  }
+
+  public List<RacingCar> createCars(String carNames) {
+    List<RacingCar> cars = new ArrayList<>();
+    for (String carName : carNames.split(DELIMITER)) {
+      String name = carName.trim();
+      cars.add(new RacingCar(name));
+    }
+    return cars;
+  }
+}
