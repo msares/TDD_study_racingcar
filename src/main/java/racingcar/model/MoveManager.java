537fe51c072c162c8fc6ca7fda7acd2bb794295c
hana.kim <hana.kim@datastreams.co.kr>
2022-11-27 18:27:19 +0900

    -feat( MoveManager , GameConstants ) : 상수 값 추가 및 매직 넘버 제거

--- src/main/java/racingcar/model/MoveManager.java
+++ src/main/java/racingcar/model/MoveManager.java
@@ -3,11 +3,12 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import static racingcar.common.GameConstants.COMPARE_VALUE;
+import static racingcar.common.GameConstants.MAX_RANDOM_NUMBER;
+import static racingcar.common.GameConstants.NUMBER_ZERO;
 
 public class MoveManager {
   public static int getRandomValue() {
-    int randomNum = Randoms.pickNumberInRange(0, 9);
-    return randomNum;
+    return Randoms.pickNumberInRange(NUMBER_ZERO, MAX_RANDOM_NUMBER);
   }
 
   public static boolean compareValue(int randomValue) {
