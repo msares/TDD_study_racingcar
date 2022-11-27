464f90d7ee5131b360d78bfc53a7e2879c8d101b
hana.kim <hana.kim@datastreams.co.kr>
2022-11-27 19:55:02 +0900

    -feat( ApplicationTest ) : 불필요 테스트 케이스 삭제

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,64 +1,11 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-import racingcar.model.RacingCarBuilder;
-import racingcar.model.RacingCar;
-
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
-    private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @Test
-    public void testCarName_split_car_names() {
-        List<RacingCar> carList = new RacingCarBuilder("hana,duna").createCars();
-        assertThat(carList.get(0).getName()).isEqualTo("hana");
-        assertThat(carList.get(1).getName()).isEqualTo("duna");
-    }
-
-    @Test
-    public void test_car_run() {
-        List<RacingCar> carList = new RacingCarBuilder("hana,duna").createCars();
-        RacingCar hanaCar = carList.get(0);
-        RacingCar dunaCar = carList.get(1);
-        hanaCar.runCar(MOVING_FORWARD);
-        dunaCar.runCar(STOP);
-        assertThat(hanaCar.getMovement()).isEqualTo("-");
-        assertThat(dunaCar.getMovement()).isEqualTo("");
-        System.out.println(hanaCar);
-        System.out.println(dunaCar);
-    }
-
-    @Test
-    void 전진_정지() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("pobi,woni", "1");
-                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-                },
-                MOVING_FORWARD, STOP
-        );
-    }
-
-    @Test
-    void 이름에_대한_예외_처리() {
-        assertSimpleTest(
-                () -> {
-                    runException("pobi,javaji");
-                    assertThat(output()).contains(ERROR_MESSAGE);
-                }
-        );
-    }
-
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
+  @Override
+  public void runMain() {
+    Application.main(new String[]{});
+  }
 }
