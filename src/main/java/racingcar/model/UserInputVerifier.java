5a96c7d7c21a3a3669b96f475438ac53c555ab26
hana.kim <hana.kim@datastreams.co.kr>
2022-11-27 16:15:34 +0900

    -feat( UserInputVerifier ) : 유저 입력값 검증을 위한 클래스 생성

---
+++ src/main/java/racingcar/model/UserInputVerifier.java
@@ -0,0 +1,31 @@
+package racingcar.model;
+
+import org.junit.platform.commons.util.StringUtils;
+
+import static racingcar.common.ErrorConstants.BLANK_STRING_ERROR;
+import static racingcar.common.ErrorConstants.CAR_NAME_OUT_OF_BOUND_ERROR;
+import static racingcar.common.ErrorConstants.WRONG_TRY_COUNT_OUT_OF_RANGE_ERROR;
+import static racingcar.common.GameConstants.MAX_CAR_NAME_LENGTH;
+import static racingcar.common.GameConstants.NUMBER_ZERO;
+
+public class UserInputVerifier {
+  public static void validateCarName(String name) {
+    if (StringUtils.isBlank(name)) {
+      throw new IllegalArgumentException(BLANK_STRING_ERROR);
+    }
+    if (name.length() > MAX_CAR_NAME_LENGTH) {
+      throw new IllegalArgumentException(CAR_NAME_OUT_OF_BOUND_ERROR + MAX_CAR_NAME_LENGTH);
+    }
+  }
+
+  public static Integer validateTryCount(String tryCount) {
+    if (StringUtils.isBlank(tryCount)) {
+      throw new IllegalArgumentException(BLANK_STRING_ERROR);
+    }
+    int tryCnt = Integer.parseInt(tryCount);
+    if (tryCnt < NUMBER_ZERO) {
+      throw new IllegalArgumentException(WRONG_TRY_COUNT_OUT_OF_RANGE_ERROR);
+    }
+    return tryCnt;
+  }
+}
