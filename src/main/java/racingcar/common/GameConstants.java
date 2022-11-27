235da0c9494249c7ee5eca100df62d31491e57a9
hana.kim <hana.kim@datastreams.co.kr>
2022-11-27 16:16:18 +0900

    -feat( ErrorConstants , GameConstants ) : 게임 진행을 위한 상수값 지정 클래스 생성

---
+++ src/main/java/racingcar/common/GameConstants.java
@@ -0,0 +1,7 @@
+package racingcar.common;
+
+public class GameConstants {
+  public static final int NUMBER_ZERO = 0;
+  public static final int MAX_CAR_NAME_LENGTH = 5;
+  public static final String DELIMITER = ",";
+}
