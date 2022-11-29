6b079ebd4c1f316508a5c2a3db616f549e34f9ae
송수정 <soojeong.song@datastreams.co.kr>
2022-11-29 12:34:06 +0900

    refactor(SplitCarTest, SplitCar): 컬렉션 클래스 생성

--- src/main/java/racingcar/model/SplitCar.java
+++ src/main/java/racingcar/model/SplitCar.java
@@ -2,14 +2,15 @@ package racingcar.model;
 
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class SplitCar {
     private static final String SPLIT_REGEX = ",";
     private static final Pattern SPLIT_PATTERN = Pattern.compile(SPLIT_REGEX);
+    private static final List<Car> carList = new ArrayList<>();
 
-    public static ArrayList<Car> splitAndCreate(String participateCars) {
-        ArrayList<Car> carList = new ArrayList<>();
+    public static List<Car> splitAndCreate(String participateCars) {
         for (String carName : splitCars(participateCars)) {
             carList.add(new Car(carName));
         }
--- src/test/java/racingcar/model/SplitCarTest.java
+++ src/test/java/racingcar/model/SplitCarTest.java
@@ -3,7 +3,7 @@ package racingcar.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,8 +11,8 @@ public class SplitCarTest {
     @Test
     @DisplayName("참여자의_이름_split하고_자동차생성")
     void 참여자의_이름_split하고_자동차생성() {
-        ArrayList<Car> car = SplitCar.splitAndCreate("SSJ,KHN");
-        assertThat(car.get(0).getCarName()).isEqualTo("SSJ");
-        assertThat(car.get(1).getCarName()).isEqualTo("KHN");
+        List<Car> car = SplitCar.splitAndCreate("SSJ,KHN");
+        assertThat(car.get(0).getName()).isEqualTo("SSJ");
+        assertThat(car.get(1).getName()).isEqualTo("KHN");
     }
 }
