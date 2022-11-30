5cec88be3c8ba3fc842d740bd1dfde2da419c206
송수정 <soojeong.song@datastreams.co.kr>
2022-11-30 12:53:51 +0900

    feat(RacingCarInput, RandomNumberTest, RandomNumber, AdvanceCount, RacingCarContoller): 랜덤값 생성, 자동차 전진횟수계산

---
+++ src/main/java/racingcar/controller/StartRacingGame.java
@@ -0,0 +1,11 @@
+package racingcar.controller;
+
+import racingcar.model.CarInfo;
+
+import java.util.List;
+
+public class StartRacingGame {
+    public static void start(List<CarInfo> carInfos) {
+    // TODO 구현
+    }
+}
